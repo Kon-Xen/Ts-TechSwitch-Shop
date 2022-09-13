86a955d737a3465d37a161c411283427bf13fb37
Konstantinos <kon.xen@:gmail.com>
2022-09-13 16:42:34 +0100

    and more classes

---
+++ Basket.cs
@@ -0,0 +1,13 @@
+using Warehouse;  
+
+namespace Shop
+{
+    class Basket
+    {
+        List<Item> items = new List<Item>();
+
+        //function to put items in the basket or goes to the shoper.
+
+    }
+
+}
