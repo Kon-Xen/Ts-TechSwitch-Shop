8b7edb1bfb148ddc58e54bc03cce35b3c32c2fca
Konstantinos <kon.xen@:gmail.com>
2022-09-13 16:34:22 +0100

    addition of classes

--- Item.cs
+++ Item.cs
@@ -1,7 +1,25 @@
 namespace Warehouse
 {
-    class Shop
+    class Item
     {
+        private string itemName;
+        private string ItemName
+        {
+            get;
+            set;
+        }
+        private decimal itemPrice;
+        private decimal ItemPrice
+        {
+            get;
+            set;
+        }
+
+        Item(string name, decimal price)
+        {
+            this.ItemPrice = price;
+            this.ItemName = name;
+        }
 
     }
 
--- Warehouse.cs
+++ Warehouse.cs
@@ -1,8 +1,16 @@
+using Checkout;
+
 namespace Warehouse
 {
     class WareHouse
     {
-        
+        List<Item> items = new List<Item>();
+        List<Transaction> transactions = new List<Transaction>();
+
+        // Check transaction is valid
+        // validate transaction's ID with the bank
+        // add transaction to internal list
+        // on confirmation add remove items from warehouse
     }
     
 }
